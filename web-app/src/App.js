e0328fce0b5117fdd8f5638e9795bdf4639ac749
Max Medina <maxmedina05@gmail.com>
2021-02-21 16:37:57 +0100

    add server code

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,14 +5,36 @@ import {
   temperatureChartDataSource,
   waterLevelDataSource,
 } from './ChartHelper'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const battery = {
-    voltage: 11.5,
-    current: 1.35,
-    temperature: 35,
-    water: [80, 80, 50],
-  }
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('/batteries')
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          setIsLoaded(true)
+          setItems(res)
+        },
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        },
+      )
+  }, [])
+
+  // const battery = {
+  //   id: 'battery_1',
+  //   voltage: 11.5,
+  //   current: 1.35,
+  //   temperature: 35,
+  //   water: [80, 80, 50],
+  // }
+  // const batteries = [battery]
 
   const batteryWaterLevelCharts = (levels) =>
     levels.map((l, i) => (
@@ -26,77 +48,86 @@ function App() {
       />
     ))
 
-  return (
-    <div className="App">
-      <h1>Monitor de Baterias</h1>
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <h1>Monitor de Baterias</h1>
 
-      <div className="batteries-container">
-        <div className="battery">
-          <h3>Batería #1</h3>
+        <div className="batteries-container">
+          {items.map((battery, idx) => (
+            <div className="battery" key={`battery_${idx}`}>
+              <h3>Batería #{idx + 1}</h3>
 
-          <div className="grid">
-            <div className="grid-item">
-              <div className="card general-info">
-                <h4 className="header">Información General</h4>
+              <div className="grid">
+                <div className="grid-item">
+                  <div className="card general-info">
+                    <h4 className="header">Información General</h4>
 
-                <div className="body">
-                  <p>
-                    <strong>Voltaje:</strong> {battery.voltage}V
-                  </p>
-                  <p>
-                    <strong>Corriente:</strong> {battery.current}A
-                  </p>
-                  <p>
-                    <strong>Temperatura:</strong> {battery.temperature}&#8451;
-                  </p>
+                    <div className="body">
+                      <p>
+                        <strong>Voltaje:</strong> {battery.voltage}V
+                      </p>
+                      <p>
+                        <strong>Corriente:</strong> {battery.current}A
+                      </p>
+                      <p>
+                        <strong>Temperatura:</strong> {battery.temperature}
+                        &#8451;
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="grid-item">
-              <div className="card voltage">
-                <h4 className="header">Voltaje</h4>
-                <div className="body">
-                  <ReactFC
-                    type="angulargauge"
-                    width="100%"
-                    height="200"
-                    dataFormat="json"
-                    dataSource={voltageChartDataSource(battery.voltage)}
-                  />
+                <div className="grid-item">
+                  <div className="card voltage">
+                    <h4 className="header">Voltaje</h4>
+                    <div className="body">
+                      <ReactFC
+                        type="angulargauge"
+                        width="100%"
+                        height="200"
+                        dataFormat="json"
+                        dataSource={voltageChartDataSource(battery.voltage)}
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="grid-item">
-              <div className="card temperature">
-                <h4 className="header">Temperatura</h4>
-                <div className="body">
-                  <ReactFC
-                    type="thermometer"
-                    width="100%"
-                    height="200"
-                    dataFormat="json"
-                    dataSource={temperatureChartDataSource(battery.temperature)}
-                  />
+                <div className="grid-item">
+                  <div className="card temperature">
+                    <h4 className="header">Temperatura</h4>
+                    <div className="body">
+                      <ReactFC
+                        type="thermometer"
+                        width="100%"
+                        height="200"
+                        dataFormat="json"
+                        dataSource={temperatureChartDataSource(
+                          battery.temperature,
+                        )}
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="card water-level">
-                <h4 className="header">Nivel de Agua</h4>
-                <div className="body">
-                  <div className="inner-container">
-                  {batteryWaterLevelCharts(battery.water)}
+                <div className="grid-item">
+                  <div className="card water-level">
+                    <h4 className="header">Nivel de Agua</h4>
+                    <div className="body">
+                      <div className="inner-container">
+                        {batteryWaterLevelCharts(battery.water)}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
